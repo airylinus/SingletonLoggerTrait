6b53a5aab1a88c8d61193f21d72f61b880da5190
作芒 <xiongzuomang@youzan.com>
2016-04-05 20:57:39 +0800

    add file

---
+++ src/SingletonLoggerTrait.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author: xiongzuomang@youzan.com
+ * @since: 16/4/5 下午6:48
+ */
+namespace airylinus\t4h;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+
+trait SingletonLoggerTrait {
+
+    private static $singletonLogger = null;
+
+    /**
+    * @param LoggerInterface $logger
+    * @return bool
+    */
+    public static function setLogger(LoggerInterface $logger) {
+        if (self::$singletonLogger === null) {
+            self::$singletonLogger = $logger;
+        }
+        return true;
+    }
+
+    /**
+     * @return LoggerInterface
+     */
+    public static function getLogger() {
+        if (self::$singletonLogger) {
+            return self::$singletonLogger;
+        } else {
+            $logger = new NullLogger();
+            return $logger;
+        }
+    }
+}
